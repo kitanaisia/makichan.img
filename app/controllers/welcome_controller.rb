c601a69eabf06b2dbede10b886a8c991d5ee0f6a
kitanaisia <kitanaisia@gmail.com>
2015-06-15 22:11:31 +0900

    Use Controller.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @user = Hash.new
+    @user[:name] = "Maki Nishikino"
+    @user[:username] = "Maki Nishikino"
+    @user[:location] = "Otonokizaka, Japan"
+    @user[:about] = "Imiwakannai"
   end
 end
